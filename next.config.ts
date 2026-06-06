accce2c6e03e7d230762c3358a6e62e1263676dd
ek <65946029+ekeer@users.noreply.github.com>
2026-06-06 17:35:42 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
+  basePath: '',
 };
 
 export default nextConfig;
